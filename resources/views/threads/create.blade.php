c9f5517f356b019468db12a708de7fcfd62d2d2d
cwang22 <wcgdream@gmail.com>
2017-04-23 17:57:50 +1000

    update create form

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -8,18 +8,42 @@
                     <div class="panel-heading">Create A new Thread</div>
 
                     <div class="panel-body">
-                        <form action="{{ url('threads') }}" method="POST">
+                        <form action="/threads" method="POST">
                             {{ csrf_field() }}
+                            @if(count($errors))
+                                <div class="form-group">
+                                    <div class="alert alert-danger" role="alert">
+                                        <ul>
+                                            @foreach($errors->all() as $error)
+                                                <li>{{$error}}</li>
+                                            @endforeach
+                                        </ul>
+                                    </div>
+                                </div>
+                            @endif
+                            <div class="form-group">
+                                <label for="channel_id">Choose a channel:</label>
+                                <select id="channel_id" name="channel_id" class="form-control" required>
+                                    <option value="">Choose one...</option>
+                                    @foreach(\App\Channel::all() as $channel)
+                                        <option value="{{$channel->id}}" {{old('channel_id') == $channel->id ? 'selected' : ''}}>
+                                            {{$channel->name}}
+                                        </option>
+                                    @endforeach
 
+                                </select>
+                            </div>
                             <div class="form-group">
                                 <label for="title">Title:</label>
-                                <input type="text" name="title" id="title" class="form-control">
+                                <input type="text" name="title" id="title" class="form-control" required value="{{old('title')}}">
                             </div>
                             <div class="form-group">
                                 <label for="body">Body:</label>
-                                <textarea name="body" id="body" cols="30" rows="10" class="form-control"></textarea>
+                                <textarea name="body" id="body" cols="30" rows="10" class="form-control" required>{{old('body')}}</textarea>
                             </div>
-                            <input type="submit" value="Publish" class="btn btn-primary">
+                            <div class="form-group">
+                                <input type="submit" value="Publish" class="btn btn-primary">
+                            </div>
                         </form>
                     </div>
                 </div>
