0ad894260fa846fd9730083faa8ab2fba4649140
cwang22 <wcgdream@gmail.com>
2017-04-29 22:07:49 +1000

    extract query form controller

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use App\User;
 
 class ProfilesController extends Controller
@@ -10,18 +11,7 @@ class ProfilesController extends Controller
     {
         return view('profiles.show')->with([
             'profileUser' => $user,
-            'activities' => $this->getActivities($user)
+            'activities' => Activity::feed($user)
         ]);
-    }
-
-    /**
-     * @param User $user
-     * @return mixed
-     */
-    public function getActivities(User $user)
-    {
-        return $user->activities()->with('subject')->latest()->get()->groupBy(function ($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
     }
 }
--- tests/Unit/ActivityTest.php
+++ tests/Unit/ActivityTest.php
@@ -2,7 +2,9 @@
 namespace Tests\Feature;
 
 use App\Activity;
+use App\Reply;
 use App\Thread;
+use Carbon\Carbon;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -27,7 +29,26 @@ class ActivityTest extends TestCase
     function it_records_activity_when_a_reply_is_created()
     {
         $this->signIn();
-        create('App\Reply');
+        create(Reply::class);
         $this->assertEquals(2, Activity::count());
+    }
+
+    /** @test */
+    public function it_fetches_feed_for_a_user()
+    {
+        $this->signIn();
+        create(Thread::class, ['user_id' => auth()->id()], 2);
+        auth()->user()->activities()->first()->update(['created_at' => Carbon::now()->subWeek()]);
+
+        $feed = Activity::feed(auth()->user());
+
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->format('Y-m-d')
+        ));
+
+        $this->assertTrue($feed->keys()->contains(
+            Carbon::now()->subWeek()->format('Y-m-d')
+        ));
+
     }
 }
