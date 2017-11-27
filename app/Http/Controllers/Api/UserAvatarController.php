7483625ab19901999b35116ad5759e9660f449d2
Chenggang Wang <chenggang.wang@designlogic.com.au>
2017-11-27 14:01:30 +1100

    bug fix

--- app/Http/Controllers/Api/UserAvatarController.php
+++ app/Http/Controllers/Api/UserAvatarController.php
@@ -3,27 +3,24 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\User;
 
 class UserAvatarController extends Controller
 {
+
     /**
      * Store uploaded avatar
      *
-     * @param User $user
-     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
+     * @return mixed
      */
-    public function store(User $user)
+    public function store()
     {
         request()->validate([
-            'avatar' => ['required', 'image']
+            'avatar' => 'required|image'
         ]);
 
-        auth()->user()->update(
-            [
-                'avatar_path' => request()->file('avatar')->store('avatars', 'public')
-            ]
-        );
+        auth()->user()->update([
+            'avatar_path' => request()->file('avatar')->store('avatars', 'public')
+        ]);
 
         return response([], 204);
     }
